4a5dee18ed7dbf1ecd1eacff0e0209ca13f9f73c
ecehavanci <ecehavanci@gmail.com>
2022-05-13 20:05:06 +0300

    firebase integrated & ready to deploy on firestore & authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:moodvicer/pages/login.dart';
 import 'package:moodvicer/values.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
